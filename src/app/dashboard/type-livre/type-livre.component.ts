0aa3172d2566bb1683dab84ec74d5de7b74ce29e
abdelkalek <abdelkalek@gmail.com>
2023-01-14 11:11:41 +0100

    SecondCommit

---
+++ src/app/dashboard/type-livre/type-livre.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-type-livre',
+  templateUrl: './type-livre.component.html',
+  styleUrls: ['./type-livre.component.scss']
+})
+export class TypeLivreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
